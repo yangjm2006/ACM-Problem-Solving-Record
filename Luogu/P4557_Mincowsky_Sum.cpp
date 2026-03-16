21420fc1080bb3a4be542046c5d15b95d60c0581
yangjm2006 <1973871746@qq.com>
2026-03-16 19:51:10 +0800

    26.3.16-training
    做了一个闵可夫斯基和例题

--- Luogu/P4557_Mincowsky_Sum.cpp
+++ Luogu/P4557_Mincowsky_Sum.cpp
@@ -1,56 +1,111 @@
-// #include<bits/stdc++.h>
-// #define ll long long
-// using namespace std;
-// const int N=1e5+100;
-// struct Point{
-//     ll x,y;
-//     Point(){}
-//     Point(ll x,ll y):x(x),y(y){}
-//     Point operator + (const Point &_){
-//         return Point(x+_.x,y+_.y);
-//     }
-//     Point operator - (const Point &_){
-//         return Point(x-_.x,y-_.y);
-//     }
-//     bool operator < (const Point &_){
-//         return x<_.x||(x==_.x&&y<_.y);
-//     }
-//     ll operator * (const Point &_){
-//         return x*_.x+y*_.y;
-//     }
-//     ll operator ^ (const Point &_){
-//         return x*_.y-y*_.x;
-//     }
-// };
-// typedef Point Vector;
-// ll len2(Vector v){
-//     return v*v;
-// }
-// double dist(Point A,Point B,Point C){
-//     return abs(((A-B)^(A-C))/sqrt(len2(B-C)));
-// }
-// int Convex_hull(Point *p,int n,Point *ch){
-//     sort(p,p+n);
-//     int cnt=0;
-//     for(int i=0;i<n;i++){
-//         while(cnt>1&&((ch[cnt-1]-ch[cnt-2])^(p[i]-ch[cnt-1]))<=0) cnt--;
-//         ch[cnt++]=p[i];
-//     }
-//     int tmp=cnt;
-//     for(int i=n-2;i>=0;i--){
-//         while(cnt>tmp&&((ch[cnt-1]-ch[cnt-2])^(p[i]-ch[cnt-1]))<=0) cnt--;
-//         ch[cnt++]=p[i];
-//     }
-//     if(cnt>1) cnt--;
-//     return cnt;
-// }
-// Point p[N],ch[N];
-// int main(){
-//     ios::sync_with_stdio(0);cin.tie(0);
-//     int n;
-//     cin>>n;
-//     for(int i=0;i<n;i++) cin>>p[i].x>>p[i].y;
-//     n=Convex_hull(p,n,ch);
-//     cout<<ans;
-//     return 0;
-// }
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+const int N=3e5+100;
+struct Point{
+    ll x,y;
+    Point(){}
+    Point(ll x,ll y):x(x),y(y){}
+    Point operator + (const Point &_){
+        return Point(x+_.x,y+_.y);
+    }
+    Point operator - (const Point &_){
+        return Point(x-_.x,y-_.y);
+    }
+    bool operator < (const Point &_){
+        return x<_.x||(x==_.x&&y<_.y);
+    }
+    ll operator * (const Point &_){
+        return x*_.x+y*_.y;
+    }
+    ll operator ^ (const Point &_){
+        return x*_.y-y*_.x;
+    }
+    ll operator / (const Point &_){
+        return x*_.y==y*_.x;
+    }
+};
+typedef Point Vector;
+int Convex_hull(Point *p,int n,Point *ch){
+    sort(p,p+n);
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        while(cnt>1&&((ch[cnt-1]-ch[cnt-2])^(p[i]-ch[cnt-1]))<=0) cnt--;
+        ch[cnt++]=p[i];
+    }
+    int tmp=cnt;
+    for(int i=n-2;i>=0;i--){
+        while(cnt>tmp&&((ch[cnt-1]-ch[cnt-2])^(p[i]-ch[cnt-1]))<=0) cnt--;
+        ch[cnt++]=p[i];
+    }
+    if(cnt>1) cnt--;
+    return cnt;
+}
+Point p1[N],ch1[N],p2[N],ch2[N],ch3[N];
+int n,m,q,tot;
+int sgn(ll x){
+    if(x==0) return 0;
+    if(x>0) return 1;
+    return -1;
+}
+bool Compare_Vector_by_Angle(Vector a,Vector b){
+    Vector IE=Vector(1,0);
+    int a1=sgn(IE*a),a2=sgn(IE^a),b1=sgn(IE*b),b2=sgn(IE^b);
+    if(a1==0&&a2<0) return 0;
+    if(b1==0&&b2<0) return 1;
+    if(a1!=b1) return a1>b1;
+    if(a1==1){
+        if(a2!=b2) return a2<b2;
+        return sgn(a^b)>=0;
+    }
+    if(a2!=b2) return a2>b2;
+    return sgn(a^b)>=0;
+}
+Vector vec[N];
+void Mincowsky(){
+    vector<Vector> v;
+    for(int i=0;i<n;i++) v.push_back(ch1[(i+1)%n]-ch1[i]);
+    for(int i=0;i<m;i++) v.push_back(ch2[(i+1)%m]-ch2[i]);
+    sort(v.begin(),v.end(),Compare_Vector_by_Angle);
+    ch3[tot++]=ch1[0]+ch2[0];
+    ch3[tot++]=ch3[0]+v[0];
+    for(int i=1;i<n+m;i++){
+        if(v[i]/v[i-1]){
+            ch3[tot-1]=ch3[tot-1]+v[i];
+        } else{
+            ch3[tot]=ch3[tot-1]+v[i];
+            tot++;
+        }
+    }
+    tot--;
+    for(int i=1;i<tot;i++) vec[i]=ch3[i]-ch3[0];
+}
+bool in_Convex_hull(Vector p){
+    if((vec[1]^p)<0||(vec[tot-1]^p)>0) return 0;
+    int l=1,r=tot-2;
+    while(l<r){
+        int mid=(l+r>>1)+1;
+        if((vec[mid]^p)>=0) l=mid;
+        else r=mid-1;
+    }
+    if(((ch3[l+1]-ch3[l])^(p+ch3[0]-ch3[l]))>=0) return 1;
+    return 0;
+}
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);
+    cin>>n>>m>>q;
+    for(int i=0;i<n;i++) cin>>p1[i].x>>p1[i].y;
+    for(int i=0;i<m;i++){
+        cin>>p2[i].x>>p2[i].y;
+        p2[i].x=-p2[i].x;p2[i].y=-p2[i].y;
+    }
+    n=Convex_hull(p1,n,ch1);
+    m=Convex_hull(p2,m,ch2);
+    Mincowsky();
+    while(q--){
+        Point p;
+        cin>>p.x>>p.y;
+        cout<<in_Convex_hull(p-ch3[0])<<'\n';
+    }
+    return 0;
+}
--- Luogu/tst.cpp
+++ Luogu/tst.cpp
@@ -1,95 +1,102 @@
-#include<bits/stdc++.h>
+#include<cstdio>
+#include<cmath>
+#include<iostream>
+#include<algorithm>
+#define N 300010 
 using namespace std;
-inline int readint(){
-	int x=0;
-	bool f=0;
-	char c=getchar();
-	while(!isdigit(c)&&c!='-') c=getchar();
-	if(c=='-'){
-		f=1;
-		c=getchar();
-	}
-	while(isdigit(c)){
-		x=x*10+c-'0';
-		c=getchar();
-	}
-	return f?-x:x;
+struct point{
+	double x,y;
+	void read(){scanf("%lf%lf",&x,&y);}
+	void write(){printf("%lf,%lf ",x,y);}
+}a[N],b[N],s1[N],s2[N],s[N],k,O,q;
+struct my_stack{
+	int t[N],len;
+	void push(int x){t[len++]=x;}
+	int operator [](int w){return t[len-w];}
+	void pop(){len--;}
+	void clear(){t[len=0]=0;}
+}t;
+point operator +(point a,point b)
+{return (point){a.x+b.x,a.y+b.y};}
+point operator -(point a,point b)
+{return (point){a.x-b.x,a.y-b.y};}
+double operator *(point a,point b)
+{return a.x*b.y-a.y*b.x;}
+bool operator ==(point a,point b)
+{return a.x==b.x&&a.y==b.y;}
+point operator ~(point a){return (point){-a.x,-a.y};}
+double sqr(double a){return a*a;}
+double calc(point a,point b)
+{return sqrt(sqr(a.x-b.x)+sqr(a.y-b.y));}
+bool cmp(point a,point b)
+{
+	double x=(a-k)*(b-k);
+	if(x==0) return calc(a,k)<calc(b,k);
+	else return x>0;
 }
-const int maxn=1e5+5;
-int n,k,x[maxn],y[maxn];
-typedef long long ll;
-ll cross(int a,int b,int c){
-	return 1ll*(x[c]-x[a])*(y[b]-y[a])-1ll*(x[b]-x[a])*(y[c]-y[a]);
+bool cmp2(point a,point b)
+{
+	double x=a*b;
+	if(x==0) return calc(a,O)<calc(b,O);
+	else return x>0;
 }
-ll dist(int a,int b){
-	return 1ll*(x[a]-x[b])*(x[a]-x[b])+1ll*(y[a]-y[b])*(y[a]-y[b]);
+double ans;
+void convex(point*p,int&n)
+{
+	int w=1;
+	for(int i=1;i<=n;i++)
+		if(p[i].y<p[w].y||(p[i].y==p[w].y&&p[i].x<p[w].x)) w=i;
+	swap(p[1],p[w]);k=p[1];
+	sort(p+2,p+n+1,cmp);
+	t.clear();
+	t.push(1),t.push(2);
+	for(int i=3;i<=n;i++)
+	{
+		while(t.len>1&&(p[i]-p[t[2]])*(p[t[1]]-p[t[2]])>=0) t.pop();
+		t.push(i);
+	}
+	n=t.len;
+	for(int i=1;i<=n;i++) p[i]=p[t.t[i-1]];
 }
-int s[maxn*2],tp;
-bool vis[maxn];
-pair<int,int> calc(){
-	vector<int> ord;
-	for(int i=1;i<=n;i++) if(!vis[i]) ord.push_back(i);
-	sort(ord.begin(),ord.end(),[](int a,int b){
-		if(x[a]!=x[b]) return x[a]<x[b];
-		return y[a]<y[b];
-	});
-	s[0]=ord[0];
-	tp=1;
-	for(int i=1;i<(int)ord.size();i++){
-		while(s[tp-1]!=ord[0]&&1ll*(y[ord[i]]-y[s[tp-1]])*(x[ord[i]]-x[s[tp-2]])>=1ll*(y[ord[i]]-y[s[tp-2]])*(x[ord[i]]-x[s[tp-1]])) tp--;
-		s[tp++]=ord[i];
+int n,m,cnt,T;
+void minkowski()
+{
+	int i,j;
+	for(i=1;i<n;i++) s1[i]=a[i+1]-a[i];
+	for(i=1;i<m;i++) s2[i]=b[i+1]-b[i];
+	s2[m]=b[1]-b[m];s1[n]=a[1]-a[n];
+	i=1,j=1;
+	s[cnt=1]=a[1]+b[1];
+	while(i<=n&&j<=m)
+	{
+		if(s1[i]*s2[j]>0) s[cnt+1]=s[cnt]+s1[i++];
+		else s[cnt+1]=s[cnt]+s2[j++];
+		cnt++;
 	}
-	for(int i=(int)ord.size()-2;i>=0;i--){
-		while(s[tp-1]!=ord.back()&&1ll*(y[s[tp-1]]-y[ord[i]])*(x[s[tp-2]]-x[ord[i]])>=1ll*(y[s[tp-2]]-y[ord[i]])*(x[s[tp-1]]-x[ord[i]])) tp--;
-		s[tp++]=ord[i];
-	}
-	tp--;
-	if(tp==2) return {s[0],s[1]};
-	int cur=0;
-	while(cross(s[tp-1],s[0],s[(cur+1)%tp])>cross(s[tp-1],s[0],s[cur])) cur=(cur+1)%tp;
-	int a=s[cur],b;
-	if(dist(s[cur],s[0])>dist(s[cur],s[tp-1])) b=s[0];
-	else b=s[tp-1];
-	for(int i=0;i<tp-1;i++){
-		while(cross(s[i],s[i+1],s[(cur+1)%tp])>cross(s[i],s[i+1],s[cur])) cur=(cur+1)%tp;
-		if(dist(s[cur],s[i])>dist(a,b)){
-			a=s[cur];
-			b=s[i];
-		}
-		if(dist(s[cur],s[i+1])>dist(a,b)){
-			a=s[cur];
-			b=s[i+1];
-		}
-	}
-	return {a,b};
+	while(i<=n) s[cnt+1]=s[cnt]+s1[i++],cnt++;
+	while(j<=m) s[cnt+1]=s[cnt]+s2[j++],cnt++;
+	cout<<"CH3--------------\n";for(int i=2;i<=cnt;i++) cout<<s[i].x<<" "<<s[i].y<<"\n";
 }
-int main(){
-	#ifdef LOCAL
-	freopen("in.txt","r",stdin);
-	freopen("out.txt","w",stdout);
-	#endif
-	n=readint();
-	k=readint();
-	for(int i=1;i<=n;i++){
-		x[i]=readint();
-		y[i]=readint();
+bool query(point x)
+{
+	if(x*s[2]>0||s[cnt]*x>0||(x*s[2]==0&&calc(x,O)>calc(s[2],O))
+		||(x*s[cnt]==0&&calc(x,O)>calc(s[cnt],O))) return 1;
+	int w=lower_bound(s+2,s+cnt+1,x,cmp2)-s-1;
+	return (x-s[w])*(s[w+1]-s[w])>0;
+}
+int main()
+{
+	scanf("%d%d%d",&n,&m,&T);
+	for(int i=1;i<=n;i++) a[i].read();
+	for(int i=1;i<=m;i++) b[i].read(),b[i]=O-b[i];
+	convex(a,n),convex(b,m);
+	cout<<"CH1--------------\n";for(int i=1;i<=n;i++) cout<<a[i].x<<" "<<a[i].y<<"\n";
+	cout<<"CH2--------------\n";for(int i=1;i<=m;i++) cout<<b[i].x<<" "<<b[i].y<<"\n";
+	minkowski(),convex(s,cnt);
+	k=s[1]; for(int i=1;i<=cnt;i++) s[i]=s[i]-k;
+	while(T--)
+	{
+		q.read();
+		printf("%d\n",!query(q-k));
 	}
-	priority_queue<ll,vector<ll>,greater<ll>> pq;
-	for(int i=0;i<min(k,n/2);i++){
-		auto res=calc();
-		int a=res.first,b=res.second;
-		pq.push(dist(a,b));
-		if((int)pq.size()>k) pq.pop();
-		vis[a]=vis[b]=1;
-		for(int j=1;j<=n;j++) if(!vis[j]){
-			pq.push(dist(a,j));
-			if((int)pq.size()>k) pq.pop();
-		}
-		for(int j=1;j<=n;j++) if(!vis[j]){
-			pq.push(dist(b,j));
-			if((int)pq.size()>k) pq.pop();
-		}
-	}
-	printf("%lld\n",pq.top());
-	return 0;
 }
