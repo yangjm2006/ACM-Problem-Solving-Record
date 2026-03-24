cbf0fbc1bf6ab4ae1ae19cb27a472d949fdadd34
yangjm2006 <1973871746@qq.com>
2026-03-24 22:29:42 +0800

    26.3.24-learning
    学了一点HTML

--- Luogu/tst.cpp
+++ Luogu/tst.cpp
@@ -1,11 +1,12 @@
-#include<cstdio>
-#include<cmath>
 #include<iostream>
-#include<algorithm>
-#define N 300010 
-using namespace std;
-int main()
-{
-	int x=10;
-	cout<<(x++)+(++x);
+#include<limits>
+void f(long) { std::cout << "long\n"; }
+// void f(long long) { std::cout << "long long\n"; }
+int main(){
+	std::cout<<sizeof(long double)<<"\n";
+	std::cout<<std::numeric_limits<long double>::digits10<<'\n';
+	f(1L);
+    f(1LL);
+    f(1); 
+	
 }
