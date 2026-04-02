1187b52ba4623c0a33b3170101f9ea4ca641d4b0
yangjm2006 <1973871746@qq.com>
2026-04-02 14:25:20 +0800

    26.4.2-training
    最小圆覆盖

--- Luogu/P1742.cpp
+++ Luogu/P1742.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#define double long double
 using namespace std;
 const int N=2e6+100;
 const double eps=1e-12;
@@ -24,7 +25,7 @@ bool in_circle(Point a,Circle b){
 }
 Point circumcenter(Point a,Point b,Point c){
     double A1=b.x-a.x,A2=c.x-a.x,B1=b.y-a.y,B2=c.y-a.y,C1=(b.x*b.x-a.x*a.x+b.y*b.y-a.y*a.y)/2,C2=(c.x*c.x-a.x*a.x+c.y*c.y-a.y*a.y)/2;
-    return (Point){(B2*C1-B1*C2)/(B2*A1-B1*A2),(A2*C1-A1*C2)/(A2*B1-A1*B2)};
+    return (Point){-(B1*C2-B2*C1)/(A1*B2-A2*B1),(A1*C2-A2*C1)/(A1*B2-A2*B1)};
 }
 Circle three_point_circle(Point a,Point b,Point c){
     Point o=circumcenter(a,b,c);
@@ -32,15 +33,15 @@ Circle three_point_circle(Point a,Point b,Point c){
 }
 int n;
 void min_enclosing_circle(){
-    q.r=-1e10;
-    for(int i=1;i<=n;i++){
+    q.o=p[1];q.r=0;
+    for(int i=2;i<=n;i++){
         if(in_circle(p[i],q)) continue;
         q.o=p[i];q.r=0;
-        for(int j=1;j<=n;j++){
+        for(int j=1;j<=i-1;j++){
             if(in_circle(p[j],q)) continue;
             q.o=middle_point(p[i],p[j]);
             q.r=distance(p[i],p[j])/2;
-            for(int k=1;k<=n;k++){
+            for(int k=1;k<=j-1;k++){
                 if(in_circle(p[k],q)) continue;
                 q=three_point_circle(p[i],p[j],p[k]);
             }
@@ -52,6 +53,6 @@ int main(){
     for(int i=1;i<=n;i++) cin>>p[i].x>>p[i].y;
     random_shuffle(p+1,p+1+n);
     min_enclosing_circle();
-    cout<<setprecision(10)<<q.r<<'\n'<<q.o.x<<" "<<q.o.y;
+    cout<<fixed<<setprecision(10)<<q.r<<'\n'<<q.o.x<<" "<<q.o.y;
     return 0;
 }
