14512a43098dac3763b504760aa92144a5facf79
yangjm2006 <1973871746@qq.com>
2026-05-16 14:18:26 +0800

    26.5.16-schoolwork-newString

--- School work/String.hpp
+++ School work/String.hpp
@@ -7,72 +7,54 @@
 class String {
 public:
 	using size_type = unsigned long;
-	constexpr static size_type npos = ULLONG_MAX;
+	constexpr static size_type npos = ULONG_MAX;
 
 private:
 	size_type m_len, m_cap;
 	char* m_buf;
-	String(const char* s, size_type len, size_type n) {
-		m_cap = n;
-		m_len = len;
-		m_buf = new char[m_cap + 1];
-		std::memcpy(m_buf, s, m_len);
-		m_buf[m_len] = '\0';
-	}
 
 public:
-	String() : m_len(0), m_cap(15), m_buf(new char[m_cap + 1]()) {}
+	String() : m_len(0), m_cap(15), m_buf(new char[16]()) {}
 
-	size_type size() const { return m_len; };
-	size_type capacity() const { return m_cap; };
-	char* data() { return m_buf; };
+	size_type size() const { return m_len; }
+	size_type capacity() const { return m_cap; }
+	char* data() { return m_buf; }
 	const char* c_str() const { return m_buf; }
 
-	~String() {
-		delete[] m_buf;
-		m_buf = nullptr;
-		m_len = m_cap = 0;
+	~String() { delete[] m_buf; }
+
+	String(const char* p) : m_len(std::strlen(p)), m_cap(m_len), m_buf(new char[m_cap + 1]) {
+		std::memcpy(m_buf, p, m_len + 1);
 	}
 
-	String(const char* s) {
-		size_type len = static_cast<size_type>(std::strlen(s));
-		m_cap = m_len = len;
-		m_buf = new char[m_cap + 1]();
-		memcpy(m_buf, s, len);
-		m_buf[m_len] = '\0';
-	}
-
-	void swap(String& other) noexcept {
+	void swap(String& other) {
 		std::swap(m_len, other.m_len);
 		std::swap(m_cap, other.m_cap);
 		std::swap(m_buf, other.m_buf);
 	}
 
 	String(const String& other)
-		: m_len(other.m_len), m_cap(other.m_cap), m_buf(new char[other.m_cap + 1]) {
-		std::memcpy(m_buf, other.m_buf, m_len);
-		m_buf[m_len] = '\0';
+		: m_len(other.m_len), m_cap(other.m_cap), m_buf(new char[m_cap + 1]()) {
+		std::memcpy(m_buf, other.m_buf, m_len + 1);
 	}
-	String(String&& other) : String() { swap(other); }
+	String(String&& other) : String() { this->swap(other); }
 
 	String& operator=(const String& other) {
-		if (this == &other) return *this;
 		String tmp(other);
-		swap(tmp);
+		this->swap(tmp);
 		return *this;
 	}
 	String& operator=(String&& other) {
-		if (this == &other) return *this;
-		String tmp;
-		other.swap(tmp);
-		swap(tmp);
+		String tmp(std::move(other));
+		this->swap(tmp);
 		return *this;
 	}
 
 	bool empty() const { return m_len == 0; }
+
 	void clear() {
 		m_len = 0;
-		m_buf[0] = 0;
+		m_buf[0] = '\0';
 	}
 
 	String& operator=(char c) {
@@ -81,42 +63,36 @@ public:
 		m_buf[1] = '\0';
 		return *this;
 	}
-	String& operator=(char* c) {
-		m_len = 1;
-		m_buf[0] = *c;
-		m_buf[1] = '\0';
-		return *this;
-	}
+	String& operator=(const char* p) { return *this = String(p); }
 
 	String(size_type n, char c) : m_len(n), m_cap(n), m_buf(new char[n + 1]) {
 		for (int i = 0; i < n; i++) m_buf[i] = c;
 		m_buf[n] = '\0';
 	}
 
-	char& operator[](size_type pos) { return m_buf[pos]; }
-	const char& operator[](size_type pos) const { return m_buf[pos]; }
+	char& operator[](size_type i) { return m_buf[i]; }
+	const char& operator[](size_type i) const { return m_buf[i]; }
 
+private:
+	String(const String& other, size_type n)
+		: m_len(other.m_len), m_cap(n), m_buf(new char[n + 1]()) {
+		std::memcpy(m_buf, other.m_buf, m_len);
+	}
+
+public:
 	void reserve(size_type n) {
-		if (n > m_cap) {
-			String tmp(this->m_buf, m_len, n);
-			swap(tmp);
-		}
+		if (m_cap < n) *this = String(*this, n);
 	}
 
 	void push_back(char c) {
 		if (m_len == m_cap) {
-			if (m_cap == 0) {
-				reserve(15);
-			} else {
+			if (m_cap == 0)
+				reserve(2);
+			else
 				reserve(2 * m_cap);
-			}
 		}
-		m_buf[m_len] = c;
-		m_len++;
+		m_buf[m_len++] = c;
 		m_buf[m_len] = '\0';
 	}
-	void pop_back() {
-		m_len--;
-		m_buf[m_len] = '\0';
-	}
+	void pop_back() { m_buf[--m_len] = '\0'; }
 };
--- School work/tst.cpp
+++ School work/tst.cpp
@@ -1,56 +1,98 @@
 #pragma once
 
-#include "Vector3d.hpp"
+#include <climits>
+#include <cstring>
+#include <utility>
 
-class Matrix3d {
+class String {
 public:
-	Vector3d row[3];
+	using size_type = unsigned long;
+	constexpr static size_type npos = ULONG_MAX;
 
-	Matrix3d() { row[0] = row[1] = row[2] = Vector3d{0, 0, 0}; }
-	Matrix3d(const double a[]) {
-		row[0] = {a[0], a[1], a[2]};
-		row[1] = {a[3], a[4], a[5]};
-		row[2] = {a[6], a[7], a[8]};
+private:
+	size_type m_len, m_cap;
+	char* m_buf;
+
+public:
+	String() : m_len(0), m_cap(15), m_buf(new char[16]()) {}
+
+	size_type size() const { return m_len; }
+	size_type capacity() const { return m_cap; }
+	char* data() { return m_buf; }
+	const char* c_str() const { return m_buf; }
+
+	~String() { delete[] m_buf; }
+
+	String(const char* p) : m_len(std::strlen(p)), m_cap(m_len), m_buf(new char[m_cap + 1]) {
+		std::memcpy(m_buf, p, m_len + 1);
 	}
 
-	const Vector3d& operator[](int id) const { return row[id]; }
-	Vector3d& operator[](int id) { return row[id]; }
+	void swap(String& other) {
+		std::swap(m_len, other.m_len);
+		std::swap(m_cap, other.m_cap);
+		std::swap(m_buf, other.m_buf);
+	}
 
-	Matrix3d& operator+=(const Matrix3d& other) {
-		for (int i = 0; i < 3; i++) row[i] += other.row[i];
+	String(const String& other)
+		: m_len(other.m_len), m_cap(other.m_cap), m_buf(new char[m_cap + 1]()) {
+		std::memcpy(m_buf, other.m_buf, m_len + 1);
+	}
+	String(String&& other) : String() { this->swap(other); }
+
+	String& operator=(const String& other) {
+		String tmp(other);
+		this->swap(tmp);
 		return *this;
 	}
-	Matrix3d operator+(const Matrix3d& other) const {
-		Matrix3d tmp(*this);
-		tmp += other;
-		return tmp;
-	}
-
-	Matrix3d& operator*=(double k) {
-		for (int i = 0; i < 3; i++) row[i] *= k;
+	String& operator=(String&& other) {
+		String tmp(std::move(other));
+		this->swap(tmp);
 		return *this;
 	}
-	Matrix3d operator*(double k) const {
-		Matrix3d tmp(*this);
-		tmp *= k;
-		return tmp;
+
+	bool empty() const { return m_len == 0; }
+
+	void clear() {
+		m_len = 0;
+		m_buf[0] = '\0';
 	}
 
-	Matrix3d operator*(const Matrix3d& other) const {
-		Matrix3d tmp;
-		for (int i = 0; i < 3; i++)
-			for (int j = 0; j < 3; j++)
-				for (int k = 0; k < 3; k++) tmp.row[i][j] += row[i][k] * other.row[k][j];
-		return tmp;
+	String& operator=(char c) {
+		m_len = 1;
+		m_buf[0] = c;
+		m_buf[1] = '\0';
+		return *this;
+	}
+	String& operator=(const char* p) { return *this = String(p); }
+
+	String(size_type n, char c) : m_len(n), m_cap(n), m_buf(new char[n + 1]) {
+		for (int i = 0; i < n; i++) m_buf[i] = c;
+		m_buf[n] = '\0';
 	}
 
-	static Matrix3d identity() { return Matrix3d((double[]){1, 0, 0, 0, 1, 0, 0, 0, 1}); }
+	char& operator[](size_type i) { return m_buf[i]; }
+	const char& operator[](size_type i) const { return m_buf[i]; }
 
-	static Matrix3d scale(double sx, double sy, double sz) {
-		return Matrix3d((double[]){sx, 0, 0, 0, sy, 0, 0, 0, sz});
+private:
+	String(const String& other, size_type n)
+		: m_len(other.m_len), m_cap(n), m_buf(new char[n + 1]()) {
+		std::memcpy(m_buf, other.m_buf, m_len);
 	}
 
-	friend Vector3d operator*(const Matrix3d& m, const Vector3d& v) {
-		return Vector3d(m.row[0] * v, m.row[1] * v, m.row[2] * v);
+public:
+	void reserve(size_type n) {
+		if (m_cap < n) *this = String(*this, n);
 	}
+
+	void push_back(char c) {
+		if (m_len == m_cap) {
+			if (m_cap == 0)
+				reserve(2);
+			else
+				reserve(2 * m_cap);
+		}
+		m_buf[m_len++] = c;
+		m_buf[m_len] = '\0';
+	}
+	void pop_back() { m_buf[--m_len] = '\0'; }
 };
