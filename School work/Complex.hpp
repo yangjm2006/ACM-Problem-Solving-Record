81d3d381e52d01356080567a91562c39a62f37e0
yangjm2006 <1973871746@qq.com>
2026-05-16 20:19:59 +0800

    26.5.16-schoolwork-update4

--- School work/Complex.hpp
+++ School work/Complex.hpp
@@ -14,14 +14,13 @@ public:
 	T real() const { return r; }
 	T imag() const { return i; }
 
-	void real(T r) { this->r = r; }
-	void imag(T i) { this->i = i; }
+	void real(T x) { r = x; }
+	void imag(T x) { i = x; }
 
-	explicit Complex(T r, T i = T{}) : r(r), i(i) {}
-
+	explicit Complex(T r, T i = T()) : r(r), i(i) {}
 	Complex(const Complex& other) = default;
 
-	Complex& operator=(const Complex& other) {
+	const Complex& operator=(const Complex& other) {
 		r = other.r;
 		i = other.i;
 		return *this;
@@ -32,54 +31,38 @@ public:
 		i += other.i;
 		return *this;
 	}
-
-	Complex operator+(const Complex& other) const {
-		return Complex<T>(this->r + other.r, this->i + other.i);
-	}
+	Complex operator+(const Complex& other) const { return Complex(r + other.r, i + other.i); }
 
 	Complex operator*(const Complex& other) const {
-		return Complex<T>(this->r * other.r - this->i * other.i,
-						  this->r * other.i + this->i * other.r);
+		return Complex(r * other.r - i * other.i, r * other.i + i * other.r);
 	}
 
-	bool operator==(const Complex& other) const;
+	friend std::istream& operator>>(std::istream& is, Complex<T>& z) {
+		T r, i;
+		char c;
+		is >> r >> i >> c;
+		z = Complex(r, i);
+		return is;
+	}
+	friend std::ostream& operator<<(std::ostream& os, const Complex& z) {
+		os << z.real();
+		if (z.imag() >= 0) {
+			os << "+";
+		}
+		os << z.imag() << "i";
+		return os;
+	}
+
+	friend Complex operator+(T a, const Complex<T>& b) { return Complex(a + b.real(), b.imag()); }
+
+	bool operator==(const Complex<float>& other) const {
+		return std::abs(r - other.r) <= 1e-4f && std::abs(i - other.i) <= 1e-4f;
+	}
+	bool operator==(const Complex<double>& other) const {
+		return std::abs(r - other.r) <= 1e-8 && std::abs(i - other.i) <= 1e-8;
+	}
+	bool operator==(const Complex<long double>& other) const {
+		return std::abs(r - other.r) <= 1e-12l && std::abs(i - other.i) <= 1e-12l;
+	}
 	bool operator!=(const Complex& other) const { return !(*this == other); }
 };
-
-template <>
-inline bool Complex<float>::operator==(const Complex<float>& other) const {
-	return std::abs(r - other.r) <= 1e-5f && std::abs(i - other.i) <= 1e-5f;
-}
-template <>
-inline bool Complex<double>::operator==(const Complex<double>& other) const {
-	return std::abs(r - other.r) <= 1e-10 && std::abs(i - other.i) <= 1e-10;
-}
-template <>
-inline bool Complex<long double>::operator==(const Complex<long double>& other) const {
-	return std::abs(r - other.r) <= 1e-15l && std::abs(i - other.i) <= 1e-15l;
-}
-
-template <typename T>
-inline std::istream& operator>>(std::istream& is, Complex<T>& c) {
-	T r, i;
-	char ch;
-	is >> r >> i >> ch;
-	c.real(r);
-	c.imag(i);
-	return is;
-}
-
-template <typename T>
-inline std::ostream& operator<<(std::ostream& os, const Complex<T>& c) {
-	os << c.real();
-	if (c.imag() >= 0) {
-		os << "+";
-	}
-	os << c.imag() << "i";
-	return os;
-}
-
-template <typename T>
-Complex<T> operator+(T r, const Complex<T>& c) {
-	return Complex<T>(c.real() + r, c.imag());
-}
--- School work/tst.cpp
+++ School work/tst.cpp
@@ -1,36 +1,68 @@
 #pragma once
 
-namespace pda {
-	template <int N = 1>
-	class Probe {
-	private:
-		inline static long counter[6] = {};
-		char dummy[N];
+#include <cmath>
+#include <iostream>
 
-	public:
-		Probe() { ++counter[0]; }
-		static int ctor_count() { return counter[0]; }
+template <typename T = double>
+class Complex {
+private:
+	T r, i;
 
-		Probe(const Probe&) { ++counter[1]; }
-		static int copy_count() { return counter[1]; }
+public:
+	Complex() : r(0), i(0) {}
 
-		Probe(Probe&&) { ++counter[2]; }
-		static int move_count() { return counter[2]; }
+	T real() const { return r; }
+	T imag() const { return i; }
 
-		Probe& operator=(const Probe&) {
-			++counter[3];
-			return *this;
+	void real(T x) { r = x; }
+	void imag(T x) { i = x; }
+
+	explicit Complex(T r, T i = T()) : r(r), i(i) {}
+	Complex(const Complex& other) = default;
+
+	const Complex& operator=(const Complex& other) {
+		r = other.r;
+		i = other.i;
+		return *this;
+	}
+
+	Complex& operator+=(const Complex& other) {
+		r += other.r;
+		i += other.i;
+		return *this;
+	}
+	Complex operator+(const Complex& other) const { return Complex(r + other.r, i + other.i); }
+
+	Complex operator*(const Complex& other) const {
+		return Complex(r * other.r - i * other.i, r * other.i + i * other.r);
+	}
+
+	friend std::istream& operator>>(std::istream& is, Complex<T>& z) {
+		T r, i;
+		char c;
+		is >> r >> i >> c;
+		z = Complex(r, i);
+		return is;
+	}
+	friend std::ostream& operator<<(std::ostream& os, const Complex& z) {
+		os << z.real();
+		if (z.imag() >= 0) {
+			os << "+";
 		}
-		static int copy_assign_count() { return counter[3]; }
+		os << z.imag() << "i";
+		return os;
+	}
 
-		Probe& operator=(Probe&&) {
-			++counter[4];
-			return *this;
-		}
-		static int move_assign_count() { return counter[4]; }
+	friend Complex operator+(T a, const Complex<T>& b) { return Complex(a + b.real(), b.imag()); }
 
-		~Probe() { ++counter[5]; }
-		static int dtor_count() { return counter[5]; }
-	};
-
-}  // namespace pda
+	bool operator==(const Complex<float>& other) const {
+		return std::abs(r - other.r) <= 1e-4f && std::abs(i - other.i) <= 1e-4f;
+	}
+	bool operator==(const Complex<double>& other) const {
+		return std::abs(r - other.r) <= 1e-8 && std::abs(i - other.i) <= 1e-8;
+	}
+	bool operator==(const Complex<long double>& other) const {
+		return std::abs(r - other.r) <= 1e-12l && std::abs(i - other.i) <= 1e-12l;
+	}
+	bool operator!=(const Complex& other) const { return !(*this == other); }
+};
