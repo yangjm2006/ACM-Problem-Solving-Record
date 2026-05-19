7500c6b82da068d11e8851e6c4ef25224f7c8223
yangjm2006 <1973871746@qq.com>
2026-05-19 17:10:14 +0800

    26.5.19-training

--- Chinese Provincial/Fujian/2025/C.cpp
+++ Chinese Provincial/Fujian/2025/C.cpp
@@ -1,77 +1,69 @@
-#include <iostream>
-#define ll long long
+#include <bits/stdc++.h>
 using namespace std;
-#define int long long
-const int N = 1e5 + 9;
-ll tr[N << 2], tag[N << 2], n, m, b[N];
-void push_up(ll u) { tr[u] = tr[u << 1] + tr[u << 1 | 1]; }
-void build(ll u, ll l, ll r) {
-	if (l == r) return tr[u] = b[l], void();
-	ll mid = l + r >> 1;
-	build(u << 1, l, mid);
-	build(u << 1 | 1, mid + 1, r);
-	push_up(u);
-}
-void push_down(ll u, ll l, ll r) {
-	ll mid = l + r >> 1;
-	tag[u << 1] += tag[u];
-	tag[u << 1 | 1] += tag[u];
-	tr[u << 1] += tag[u] * (mid - l + 1);
-	tr[u << 1 | 1] += tag[u] * (r - mid);
-	tag[u] = 0;
-}
-void add(ll u, ll l, ll r, ll L, ll R, ll x) {
-	if (l >= L && r <= R) return tr[u] += (r - l + 1) * x, tag[u] += x, void();
-	push_down(u, l, r);
-	ll mid = l + r >> 1;
-	if (mid >= L) add(u << 1, l, mid, L, R, x);
-	if (mid + 1 <= R) add(u << 1 | 1, mid + 1, r, L, R, x);
-	push_up(u);
-}
-ll query(ll u, ll l, ll r, ll L, ll R) {
-	if (l >= L && r <= R) return tr[u];
-	push_down(u, l, r);
-	ll mid = l + r >> 1, res = 0;
-	if (mid >= L) res += query(u << 1, l, mid, L, R);
-	if (mid + 1 <= R) res += query(u << 1 | 1, mid + 1, r, L, R);
-	push_up(u);
-	return res;
-}
-
-void solve() {
-	cin >> n >> m;
-	int a[n + 1];
-	for (int i = 1; i <= n; i++) cin >> a[i];
-	int k;
-	int s[m + 1];
-	int t[m + 1];
-	// for (int i = 1; i <= m; i++) {
-	cin >> k;
-	// }
-	for (int i = 1; i <= m; i++) {
-		cin >> s[i] >> t[i];
+const int N = 1e6 + 100;
+const int INF = 1e9;
+int a[N];
+struct node {
+	int col, len;
+	node(int c, int l) : col(c), len(l) {}
+	bool operator==(const node& other) { return col == other.col && len == other.len; }
+};
+int n;
+bool check(int k) {
+	vector<node> v;
+	for (int i = 1; i <= n; i++) {
+		int x = a[i] >= k ? 1 : 0;
+		if (v.size() && v[v.size() - 1].col == x) {
+			v[v.size() - 1].len++;
+			if (x == 0) {
+				++((--v[v.size() - 1].len) %= 2);
+			}
+		} else {
+			v.push_back(node(x, 1));
+		}
 	}
-	for (int i = 1; i < n; i++) {
-		b[i] = max(0ll, a[i + 1] - a[i]);
+	vector<node> vec;
+	for (auto x : v) {
+		if (vec.size() >= 2 && x == node(0, 2) && vec[vec.size() - 1] == node(1, 1) &&
+			vec[vec.size() - 2] == node(0, 2)) {
+			vec.pop_back();
+			vec.pop_back();
+			vec.push_back(node(0, 1));
+		} else if (vec.size() >= 1 && x == node(0, 1) && vec[vec.size() - 1] == node(1, 1)) {
+			vec.pop_back();
+		} else {
+			vec.push_back(x);
+		}
 	}
-	b[n] = 0;
-	build(1, 1, n);
-	for (int i = 1; i <= m; i++) {
-		if (s[i] == t[i])
-			cout << k << '\n';
+	int cnt1 = 0, cnt0 = 0;
+	for (auto [col, len] : vec) {
+		if (col)
+			cnt1 += len;
 		else
-			cout << k + query(1, 1, n, s[i], t[i] - 1) << '\n';
+			cnt0 += len;
 	}
+	return cnt1 > cnt0;
 }
-
-signed main() {
+void __() {
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+	int l = 1, r = INF;
+	while (l < r) {
+		int mid = (l + r >> 1) + 1;
+		if (check(mid))
+			l = mid;
+		else
+			r = mid - 1;
+	}
+	cout << l << '\n';
+}
+int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	cout.tie(0);
-	int t = 1;
-	// cin >> t;
-	while (t--) {
-		solve();
-	}
+	int T;
+	cin >> T;
+	while (T--) __();
 	return 0;
 }
